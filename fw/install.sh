d290f7727813f9be29a6b81070e1140e1ff1d3c9
Tim Janus <tim.janus@udo.edu>
2012-02-14 12:21:16 +0100

    update documentation "How to Install Angerona" and remove not needed files

--- fw/install.sh
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -Dfile=lib/jspf.core-1.0.2.jar -DgroupId=net.xeoh -DartifactId=jspf-core -Dversion=1.0.2 -Dpackaging=jar -DupdateRelaseInfo=true
-mvn install:install-file -Dfile=lib/Beenuts.jar -DgroupId=net.sf -DartifactId=beenuts -Dversion=0.1.97 -Dpackaging=jar -DupdateReleaseInfo=true
