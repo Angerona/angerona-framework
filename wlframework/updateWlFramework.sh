1dbadf28d26d26c7c8d9a39bdd55569eadb8b40d
Tim Janus <tim.janus@udo.edu>
2012-02-14 11:46:12 +0100

    remove the hard dependency to the wlframework library wlframework will be developed in its own svn repository
    adapt the pom.xml to use the Angerona Maven repository
    There you can get the wlframework artifacts now.

--- wlframework/updateWlFramework.sh
+++
@@ -1,8 +0,0 @@
-mkdir tlib
-for lib in "WlWindows" "WlConfiguration" "WlMacWidgets" "WlPreview" "WlResourceManager" "WlUtilities"; do
-echo $lib
-cd $lib/bin/
-jar cf ../../tlib/$lib.jar ./*
-cd ../../
-mvn install:install-file -Dfile=tlib/$lib.jar -DgroupId=com.whiplash -DartifactId=$lib -Dversion=0.1.22 -Dpackaging=jar -DupdateReleaseInfo=true
-done
