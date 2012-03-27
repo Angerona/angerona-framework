e9de5bcbad2216f9f63fbbc2f44e0814091db088
Tim Janus <tim.janus@udo.edu>
2012-03-27 17:55:12 +0200

    add nearly empty knowhow plugin

---
+++ knowhow/src/main/java/angerona/fw/knowhow/KBBeliefbase.java
@@ -0,0 +1,11 @@
+package angerona.fw.knowhow;
+
+import angerona.fw.logic.asp.AspBeliefbase;
+
+/**
+ * Extends a generic ASP Beliefbase with know-how.
+ * @author Tim Janus
+ */
+public class KBBeliefbase extends AspBeliefbase {
+	
+}
---
+++ knowhow/src/main/java/angerona/fw/knowhow/KnowHowStatement.java
@@ -0,0 +1,11 @@
+package angerona.fw.knowhow;
+
+import java.util.Vector;
+
+import net.sf.tweety.logicprogramming.asplibrary.syntax.Atom;
+
+public class KnowHowStatement {
+	private Atom target;
+	private Vector<Atom> subTargets;
+	private Vector<Atom> subConditions;
+}
