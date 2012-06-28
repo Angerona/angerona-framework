0cd78f3853c8868179721d72c44a662f18f6a3af
Tim Janus <tim.janus@udo.edu>
2012-06-28 14:35:37 +0200

    use the javacc secret parser there is a problem because the javacc parser uses the firstorderlogic parser and there is no ELPSignature which can be translated into a SymbolSet yet.

--- fw/src/main/java/angerona/fw/logic/Secret.java
+++ fw/src/main/java/angerona/fw/logic/Secret.java
@@ -14,28 +14,40 @@ import net.sf.tweety.logics.firstorderlogic.syntax.FolFormula;
  * his wife Alice to know that he has an affair with Claire. Also
  * he does not want Claire to know that he has children.
  * 
+ * @remark
  * In Angerona Bobs data has the Beliefbases B_A and B_C
  * representing the view on Alice or Claire. The reasoning operators
  * linked to these beliefbases are used to instead one defined in
  * this data-strcuture.
+ * TODO: Change this
  * 
  * @author Tim Janus
  */
 public class Secret implements Cloneable {
+	public static final String DEFAULT_BELIEFCHANGE = "__DEFAULT__";
+	
 	/** name of the agent who should not get the information */
 	private String name;
 	
 	/** formula representing the confidential information */
 	private FolFormula information;
 	
+	/** the java class name which should be used to proof this secret */
+	private String beliefChangeClassName;
 	/**
 	 * Ctor: Generates a new confidential target with the given parameters.
 	 * @param name					name of the agent who should not get the information
 	 * @param information			formula representing the confidential information
 	 */
 	public Secret(String name, FolFormula information) {
+		this(name, information, DEFAULT_BELIEFCHANGE);
+	}
+	
+	public Secret(String name, FolFormula information,
+			String changeClass) {
 		this.name = name;
 		this.information = information;
+		this.beliefChangeClassName = changeClass;
 	}
 	
 	/**	@return name of the agent who should not get the information */
@@ -46,6 +58,15 @@ public class Secret implements Cloneable {
 	/** @return formula representing the confidential information */
 	public Formula getInformation() {
 		return information;
+	}
+	
+	/** 
+	 * @return 	the full java class name which should be used for revision 
+	 *			might be DEFAULT_CHANGEBELIEF with the meaning the default
+	 *			change operator of the beliefbase should be used.
+	 */
+	public String getBeliefChangeClassName() {
+		return beliefChangeClassName;
 	}
 	
 	@Override
