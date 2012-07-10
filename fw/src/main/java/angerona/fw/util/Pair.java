a5cedd83bb040bd5bf94660ef567bf4d3e7111c4
Tim Janus <tim.janus@udo.edu>
2012-07-10 16:35:33 +0200

    fix null pointer exception, hashCode was not implemented in Pair so the finding in a HashMap was not functional for the pair

--- fw/src/main/java/angerona/fw/util/Pair.java
+++ fw/src/main/java/angerona/fw/util/Pair.java
@@ -18,4 +18,21 @@ public class Pair<TF, TS> {
 		this.first = first;
 		this.second = second;
 	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if(! (other instanceof Pair)) return false;
+		Pair<TF, TS> op = (Pair<TF, TS>)other;
+		boolean eqF = (first == null && op.first == null) ||
+						first != null && first.equals(op.first);
+		boolean eqS = (second == null && op.second == null) ||
+						(second != null && second.equals(op.second));
+	
+		return eqF && eqS;
+	}
+	
+	@Override 
+	public int hashCode() {
+		return (first != null ? first.hashCode() : 1) + (second != null ? second.hashCode() : 2);
+	}
 }
