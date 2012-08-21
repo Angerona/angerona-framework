743c6d30e499d5b471fc54666329e55d6779a9ca
Tim Janus <tim.janus@udo.edu>
2012-08-21 16:26:52 +0200

    use java formatter on daniels code

--- dummy/src/main/java/angerona/fw/operators/def/MaryIntentionUpdateOperator.java
+++ dummy/src/main/java/angerona/fw/operators/def/MaryIntentionUpdateOperator.java
@@ -10,124 +10,120 @@ import angerona.fw.Agent;
 import angerona.fw.Intention;
 import angerona.fw.Skill;
 import angerona.fw.Subgoal;
-import angerona.fw.logic.SecrecyStrengthPair;
+import angerona.fw.logic.Secret;
 import angerona.fw.operators.BaseIntentionUpdateOperator;
 import angerona.fw.operators.parameter.IntentionUpdateParameter;
+import angerona.fw.util.Pair;
+
 /**
-* The intention update operator suitable for the "Mary Courtroom Scenario"
-* The previous intention update operator chose the first intentino not to violate secrecy
-* This operator considers all options at once and then chooses an optimal one
-* It picks the optimal one by assigning a cost to weakening secrets and to telling lies
-* For further use of the operator, the operator should be made to distinguish between all options available in particular plans.
-* The operator currently chooses from all options from all plans.
-* @author Daniel Dilger
-*/
-public class MaryIntentionUpdateOperator extends BaseIntentionUpdateOperator{
-	
+ * The intention update operator suitable for the "Mary Courtroom Scenario" The
+ * previous intention update operator chose the first intentino not to violate
+ * secrecy This operator considers all options at once and then chooses an
+ * optimal one It picks the optimal one by assigning a cost to weakening secrets
+ * and to telling lies For further use of the operator, the operator should be
+ * made to distinguish between all options available in particular plans. The
+ * operator currently chooses from all options from all plans.
+ * 
+ * @author Daniel Dilger
+ */
+public class MaryIntentionUpdateOperator extends BaseIntentionUpdateOperator {
 
 	/** reference to the logback instance used for logging */
-	private static Logger LOG = LoggerFactory.getLogger(IntentionUpdateOperator.class);
-	
-	private boolean intentionOutdated(Intention intention)
-	{
+	private static Logger LOG = LoggerFactory
+			.getLogger(IntentionUpdateOperator.class);
+
+	private boolean intentionOutdated(Intention intention) {
 		return false;
 	}
-	private boolean isLie(Intention intention)
-	{
-		
-		if(intention.getHonestyStatus())
-		{
+
+	private boolean isLie(Intention intention) {
+
+		if (intention.getHonestyStatus()) {
 			return false;
 		}
 		return true;
 	}
-	private double lyingCost(Intention intention)
-	{
+
+	private double lyingCost(Intention intention) {
 		double estimate = 0.5;
-		report(intention+" <b> 'dontKnow' is a lie </b>. Estimated cost equal to weakening secret by "+estimate);
+		report(intention
+				+ " <b> 'dontKnow' is a lie </b>. Estimated cost equal to weakening secret by "
+				+ estimate);
 		return estimate;
 	}
-	private double secrecyWeakeningCost(List<SecrecyStrengthPair> weakenings)
-	{
+
+	private double secrecyWeakeningCost(List<Pair<Secret, Double>> weakenings) {
 		double total = 0.0;
-		for (SecrecyStrengthPair pair : weakenings)
-		{
-			total = total + pair.getDegreeOfWeakening();
+		for (Pair<Secret, Double> pair : weakenings) {
+			total = total + pair.second;
 		}
 		return total;
 	}
-	
+
 	/**
 	 * A more elegant solution would be to use Collections.max
 	 */
-	private Intention minimalCosting(List<Intention> intentions)
-	{
-		if (intentions == null || intentions.size() == 0)
-		{
+	private Intention minimalCosting(List<Intention> intentions) {
+		if (intentions == null || intentions.size() == 0) {
 			return null;
 		}
 		Intention minIntent = intentions.get(0);
 		double minCost = minIntent.getCost();
-		for (Intention intent : intentions)
-		{
+		for (Intention intent : intentions) {
 			double curCost = intent.getCost();
-			if(curCost < minCost)
-			{
+			if (curCost < minCost) {
 				minCost = curCost;
 				minIntent = intent;
 			}
 		}
 		return minIntent;
 	}
+
 	@Override
 	protected Intention processInt(IntentionUpdateParameter param) {
 		LOG.info("Run Mary-Intention-Update");
 		Agent ag = param.getPlan().getAgent();
-		List <Intention> atomicIntentions = new LinkedList<Intention>();
-		//Loop needs to be changed so that only options from the same plan are considered
-		for(Subgoal plan : param.getPlan().getPlans()) {
-			for(int i=0; i<plan.getNumberOfStacks(); ++i) {
-				if(plan.peekStack(i).isAtomic()) {
+		List<Intention> atomicIntentions = new LinkedList<Intention>();
+		// Loop needs to be changed so that only options from the same plan are
+		// considered
+		for (Subgoal plan : param.getPlan().getPlans()) {
+			for (int i = 0; i < plan.getNumberOfStacks(); ++i) {
+				if (plan.peekStack(i).isAtomic()) {
 					Intention intention = plan.peekStack(i);
 					intention.setRealRun(false);
-					if(intentionOutdated(intention)) 
-					{
+					if (intentionOutdated(intention)) {
 						continue;
 					}
-					report("Performing mental-action applying: '"+intention+"'", ag);
-					if(isLie(intention))
-					{
-						//add return value of lyingCost(intention) to intention
+					report("Performing mental-action applying: '" + intention
+							+ "'", ag);
+					if (isLie(intention)) {
+						// add return value of lyingCost(intention) to intention
 						double cost = lyingCost(intention);
 						intention.setCost(cost);
 						atomicIntentions.add(intention);
-					}
-					else
-					{
+					} else {
 						intention.run();
-						Skill sk = (Skill)intention;
-						
-						List<SecrecyStrengthPair> weakenings = sk.getWeakenings();
-						if(weakenings != null) {
+						Skill sk = (Skill) intention;
+
+						List<Pair<Secret, Double>> weakenings = sk.violates()
+								.getPairs();
+						if (weakenings != null) {
 							double cost = secrecyWeakeningCost(weakenings);
 							intention.setCost(cost);
 							atomicIntentions.add(intention);
-							
+
 						}
 					}
-					
+
 				}
 			}
 		}
-		if(atomicIntentions.size() == 0)
-		{
+		if (atomicIntentions.size() == 0) {
 			report("No atomic step candidate found.", ag);
 			return null;
-		}
-		else
-		{
+		} else {
 			Intention min = minimalCosting(atomicIntentions);
-			ag.setWeakenings(((Skill) min).getWeakenings());
+			ag.setWeakenings(((Skill) min).violates());
 			return min;
 		}
 	}
--- dummy/src/main/java/angerona/fw/operators/def/MaryUpdateBeliefsOperator.java
+++ dummy/src/main/java/angerona/fw/operators/def/MaryUpdateBeliefsOperator.java
@@ -11,77 +11,84 @@ import angerona.fw.comm.Answer;
 import angerona.fw.comm.Query;
 import angerona.fw.logic.Beliefs;
 import angerona.fw.logic.ConfidentialKnowledge;
-import angerona.fw.logic.SecrecyStrengthPair;
 import angerona.fw.logic.Secret;
 import angerona.fw.operators.BaseUpdateBeliefsOperator;
 import angerona.fw.operators.parameter.UpdateBeliefsParameter;
+import angerona.fw.util.Pair;
+
 /**
- * Update Beliefs Operator which enables the weakening of secrets.
- * The major difference between this operator and the previous belief update operator is that this one records the degree by which an agent has weakened its secrets by its actions.
+ * Update Beliefs Operator which enables the weakening of secrets. The major
+ * difference between this operator and the previous belief update operator is
+ * that this one records the degree by which an agent has weakened its secrets
+ * by its actions.
+ * 
  * @author Dilger, Janus
- *
+ * 
  */
 public class MaryUpdateBeliefsOperator extends BaseUpdateBeliefsOperator {
 	/** reference to the logback instance used for logging */
-	private static Logger LOG = LoggerFactory.getLogger(UpdateBeliefsOperator.class);
+	private static Logger LOG = LoggerFactory
+			.getLogger(UpdateBeliefsOperator.class);
+
 	@Override
 	protected Beliefs processInt(UpdateBeliefsParameter param) {
 		LOG.info("Run Mary-Update-Beliefs-Operator");
-		ConfidentialKnowledge conf = param.getAgent().getComponent(ConfidentialKnowledge.class);
-		
+		ConfidentialKnowledge conf = param.getAgent().getComponent(
+				ConfidentialKnowledge.class);
+
 		Beliefs reval = param.getAgent().getBeliefs();
 		String id = param.getAgent().getAgentProcess().getName();
 		String out = "Update-Beliefs: ";
-		if(param.getPerception() instanceof Answer) {
-			Answer naa = (Answer)param.getPerception();
+		if (param.getPerception() instanceof Answer) {
+			Answer naa = (Answer) param.getPerception();
 			out += "Answer ";
-			out += (naa.getSenderId().compareTo(id) == 0 ? "as sender (view)" : "as receiver (world)");
-			
-			
-			List<SecrecyStrengthPair> weakenings = param.getAgent().getWeakenings();
-			
-			
+			out += (naa.getSenderId().compareTo(id) == 0 ? "as sender (view)"
+					: "as receiver (world)");
+
+			List<Pair<Secret, Double>> weakenings = param.getAgent()
+					.getWeakenings().getPairs();
+
 			BaseBeliefbase bb = null;
-			if(id.compareTo(naa.getReceiverId()) == 0) {
+			if (id.compareTo(naa.getReceiverId()) == 0) {
 				bb = reval.getWorldKnowledge();
-			} else if (id.compareTo(naa.getSenderId()) == 0){
-				bb = param.getAgent().getBeliefs().getViewKnowledge().get(naa.getReceiverId());
+			} else if (id.compareTo(naa.getSenderId()) == 0) {
+				bb = param.getAgent().getBeliefs().getViewKnowledge()
+						.get(naa.getReceiverId());
 			}
-			
+
 			bb.addKnowledge(naa);
 
-			for(Secret secret : conf.getTargets())  {
+			for (Secret secret : conf.getTargets()) {
 
-				if(weakenings == null)
-				{
+				if (weakenings == null) {
 					continue;
 				}
-					
-				for (SecrecyStrengthPair sPair : weakenings)
-				{
-					
-					if(secret.getInformation().toString().equals(sPair.getSecret().getInformation().toString()))
-					{
-						Map <String, String> map =  secret.getReasonerParameters();
+
+				for (Pair<Secret, Double> sPair : weakenings) {
+					if (secret.alike(sPair.first)) {
+						Map<String, String> map = secret
+								.getReasonerParameters();
 						double oldD = Double.parseDouble(map.get("d"));
-						double newD = oldD - sPair.getDegreeOfWeakening();
+						double newD = oldD - sPair.second;
 						map.put("d", new Double(newD).toString());
-						secret.setReasonerParameters(map); 
+						secret.setReasonerParameters(map);
 					}
 				}
 			}
 			report(out, bb);
-		} else if(param.getPerception() instanceof Query) {
-			
-			Query naq = (Query)param.getPerception();
+		} else if (param.getPerception() instanceof Query) {
+
+			Query naq = (Query) param.getPerception();
 			out += "Query ";
-			out += (naq.getSenderId().compareTo(id) == 0 ? "as sender (might be an error)" : "as receiver (view)");
-			
+			out += (naq.getSenderId().compareTo(id) == 0 ? "as sender (might be an error)"
+					: "as receiver (view)");
+
 			report(out, param.getAgent());
 		} else {
-			LOG.warn("Update-Operator: Cant handle perception of type: " + param.getPerception().getClass().getName());
+			LOG.warn("Update-Operator: Cant handle perception of type: "
+					+ param.getPerception().getClass().getName());
 		}
-		
+
 		return reval;
 	}
 
