3871d58ee34b75bfff992b9285d600d6653fee9c
wierzoch <pia.wierzoch@tu-dortmund.de>
2013-02-27 12:17:26 +0100

    implement methods to translate the view for the TheoremSolver

--- DefendingAgent/src/main/java/angerona/fw/DefendingAgent/operators/def/Censor.java
+++ DefendingAgent/src/main/java/angerona/fw/DefendingAgent/operators/def/Censor.java
@@ -1,5 +1,11 @@
 package angerona.fw.DefendingAgent.operators.def;
 
+import java.util.Collection;
+import java.util.Set;
+
+import net.sf.tweety.logics.conditionallogic.syntax.Conditional;
+import net.sf.tweety.logics.propositionallogic.PlBeliefSet;
+import net.sf.tweety.logics.propositionallogic.syntax.PropositionalFormula;
 import angerona.fw.Action;
 import angerona.fw.Agent;
 import angerona.fw.DefendingAgent.comm.Revision;
@@ -27,5 +33,92 @@ public class Censor {
 	public Action processRevision(Agent ag, Revision rev) {
 		return new Answer(ag, rev.getReceiverId(), rev.getProposition(), AnswerValue.AV_REJECT);
 
+	}
+	
+	/**
+	 * Create a BelifeBase out of the View on the attacking agent
+	 * @param positiveConditionalBeliefs
+	 * @param negativeConditionalBeliefs
+	 * @param beliefSet
+	 * @return a String[] that represent the approximation of the knowledge of the attacking agent
+	 * @see[1] Biskup, Joachim and Tadros, Cornelia. Revising Belief without Revealing Secrets
+	 */
+	private String[] makeBelifeBase(Set<Conditional> positiveConditionalBeliefs, Set<Conditional> negativeConditionalBeliefs, PlBeliefSet beliefSet){
+		int n = 0, i=0;
+		n += positiveConditionalBeliefs.size() + negativeConditionalBeliefs.size() + beliefSet.size();
+		String[] belifeBase = new String[n];
+		for(Conditional a: positiveConditionalBeliefs){
+			belifeBase[i] = translate(a.getPremise()) + "=>" + translate(a.getConclusion());
+			i++;
+		}
+		for(Conditional a: negativeConditionalBeliefs){
+			belifeBase[i] = "neg ("+ translate(a.getPremise()) + "=>" + translate(a.getConclusion()) +")";
+			i++;
+		}
+		for(PropositionalFormula a: beliefSet){
+			belifeBase[i] = "neg (" + translate(a) + "=> false)";
+			i++;
+		}
+		return belifeBase;
+	}
+	
+	/**
+	 * Translate a Collection of PropositionalFormulas by using the 
+	 * method translate for PropositionalFormulas and connect the Formulas with "and"
+	 * @param formulas
+	 * 	Collection of PropositionalFormulas to translate
+	 * @return a String where the operators are changed to "or" "and" "neg" and the formulas are 
+	 * Connected with "and"
+	 */
+	private String translate(Collection<PropositionalFormula> formulas){
+		String result = "";
+		for(PropositionalFormula a : formulas){
+			if(result.equalsIgnoreCase("")){
+				result = translate(a); 
+			}else{
+				result = result + " and " + translate(a); 
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Translate PropositionalForumla to a String that the TheoremSolver 
+	 * can understand by changing "||" "&&" "-" to "or" "and" "neg"
+	 * @param formula
+	 * 	the PropositionalFormula to translate
+	 * @return a String where the operators are changed to "or" "and" "neg"
+	 */
+	private String translate(PropositionalFormula formula){
+		String[] b;
+		String result = "";
+		//change || to or
+		b = formula.toString().split("||");
+		if(b.length > 1){
+			for(int i = 0; i<b.length-1;i++){
+				result += b[i] + " or ";
+			}
+		}
+		result += b[b.length-1];
+		//change && to and
+		b = result.split("&&");
+		result = "";
+		if(b.length > 1){
+			for(int i = 0; i<b.length-1;i++){
+			result += b[i] + " and ";
+			}
+		}
+		result += b[b.length-1];
+		//change - to neg
+		b = result.split("-");
+		result = "";
+		if(b.length > 1){
+			for(int i = 0; i<b.length-1;i++){
+			result += b[i] + " neg ";
+			}
+		}
+		result += b[b.length-1];
+		
+		return "( "+ result +" )";
 	}
 }
