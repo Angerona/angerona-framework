5cfef9779244620e03940a8db4b3ed22571fbf93
Sebastian Homann <sebastian.homann@tu-dortmund.de>
2013-11-19 17:17:31 +0100

    added runtime exception in cases when the klmlean solver stops with a memory error uncommented refinement-method in view class

--- DefendingAgent/src/main/java/com/github/angerona/fw/defendingagent/Prover/SICStusException.java
+++ DefendingAgent/src/main/java/com/github/angerona/fw/defendingagent/Prover/SICStusException.java
@@ -1,5 +1,13 @@
 package com.github.angerona.fw.defendingagent.Prover;
 
-public class SICStusException extends Exception{
+public class SICStusException extends Exception {
+
+	public SICStusException(String string) {
+		super(string);
+	}
+	
+	public SICStusException() {
+		super();
+	}
 
 }
