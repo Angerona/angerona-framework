0181cea40aaca7375e22342bf639ca0d382822ec
B42ribi <m.barbi@geniusbytes.com>
2014-09-05 12:53:10 +0200

    tidied island update belief operator

--- motivation/src/main/java/com/github/angerona/fw/motivation/island/operators/IslandUpdateBeliefsOperator.java
+++ motivation/src/main/java/com/github/angerona/fw/motivation/island/operators/IslandUpdateBeliefsOperator.java
@@ -7,27 +7,28 @@ import com.github.angerona.fw.motivation.island.IslandPerception;
 import com.github.angerona.fw.operators.BaseUpdateBeliefsOperator;
 import com.github.angerona.fw.operators.parameter.EvaluateParameter;
 
+/**
+ * 
+ * @author Stefan Roetner
+ * 
+ */
 public class IslandUpdateBeliefsOperator extends BaseUpdateBeliefsOperator {
 
 	@Override
 	protected Beliefs processImpl(EvaluateParameter param) {
 		Beliefs beliefs = param.getBeliefs();
-		Beliefs oldBeliefs = (Beliefs)param.getBeliefs().clone();
-//		String id = param.getAgent().getName();
-//		
-//		
+		Beliefs oldBeliefs = (Beliefs) param.getBeliefs().clone();
 		BaseBeliefbase bb = beliefs.getWorldKnowledge();
 		if (param.getAtom() instanceof IslandPerception) {
 			IslandPerception p = (IslandPerception) param.getAtom();
 			bb.addKnowledge(p);
 			param.report("Reveived Perception: " + p.toString(), bb);
 		} else {
-			param.report("Update-Operator: Can't handle perception of type: " + param.getAtom().getClass().getName());param.report("Update-Operator: Cant handle perception of type: " + param.getAtom().getClass().getName());
+			param.report("Update-Operator: Can't handle perception of type: " + param.getAtom().getClass().getName());
 		}
-		
-		param.getAgent().onUpdateBeliefs((Perception)param.getAtom(), oldBeliefs);
+
+		param.getAgent().onUpdateBeliefs((Perception) param.getAtom(), oldBeliefs);
 		return beliefs;
 	}
 
 }
-
