1274c80b9fe5bd44f40e9697c9df8ffd28436535
Manuel <manuel.barbi@tu-dortmund.de>
2015-09-11 15:13:58 +0200

    simplified filter operator

--- simple/src/main/java/com/github/angerona/fw/simple/operators/FilterOperator.java
+++ simple/src/main/java/com/github/angerona/fw/simple/operators/FilterOperator.java
@@ -25,96 +25,60 @@ public abstract class FilterOperator extends BaseIntentionUpdateOperator {
 		Desires desireComp = param.getAgent().getComponent(Desires.class);
 		PlanComponent plans = param.getActualPlan();
 
+		if (plans == null) {
+			param.report("no plan component found");
+			return null;
+		}
+
 		Desire pursued = null;
 		if (!plans.getPlans().isEmpty()) {
 			pursued = plans.getPlans().get(0).getFulfillsDesire();
 		}
 
-		if (desireComp != null) {
-			Set<Desire> desires = desireComp.getDesires();
+		if (desireComp == null) {
+			param.report("no desire component found");
+			return null;
+		}
 
-			Map<Desire, Subgoal> intentions = new LinkedHashMap<>();
-			for (Subgoal sub : plans.getPlans()) {
-				if (desires.contains(sub.getFulfillsDesire())) {
-					intentions.put(sub.getFulfillsDesire(), sub);
-				}
+		Set<Desire> desires = desireComp.getDesires();
+
+		Map<Desire, Subgoal> intentions = new LinkedHashMap<>();
+		for (Subgoal sub : plans.getPlans()) {
+			if (desires.contains(sub.getFulfillsDesire())) {
+				intentions.put(sub.getFulfillsDesire(), sub);
 			}
+		}
 
-			plans.clear();
-			process(new ParamBundle(plans, desires, pursued, intentions, param));
+		plans.clear();
+		Desire goal = choose(desires, pursued, param);
 
-			for (Subgoal sub : intentions.values()) {
-				plans.addPlan(sub);
-			}
+		if (goal != null) {
+			Subgoal tmp = intentions.remove(goal);
+			plans.addPlan((tmp != null) ? tmp : new Subgoal(param.getAgent(), goal));
+			param.report("agent has chosen " + goal);
+		}
+
+		for (Subgoal sub : intentions.values()) {
+			plans.addPlan(sub);
 		}
 
 		return null;
 	}
 
-	protected abstract void process(ParamBundle param);
-
-	protected void choose(Desire desire, ParamBundle param) {
-		Subgoal tmp = param.getIntentions().remove(desire);
-		param.getPlans().addPlan((tmp != null) ? tmp : new Subgoal(param.getParam().getAgent(), desire));
-		param.getParam().report("agent has chosen " + desire);
-	}
+	/**
+	 * @param options
+	 *            a set of all desires, the agent currently has
+	 * @param pursued
+	 *            the currently pursued desire, if any
+	 * @param param
+	 *            the original plan-parameter
+	 * @return the desire the agent will pursue
+	 */
+	protected abstract Desire choose(Set<Desire> options, Desire pursued, PlanParameter param);
 
 	@Override
 	protected PlanParameter getEmptyParameter() {
 		return new SimplePlanParameter();
-	}
-
-	protected class ParamBundle {
-
-		private final PlanComponent plans;
-		private final Set<Desire> desires;
-		private final Desire pursued;
-		private final Map<Desire, Subgoal> intentions;
-		private final PlanParameter param;
-
-		public ParamBundle(PlanComponent plans, Set<Desire> desires, Desire pursued, Map<Desire, Subgoal> intentions, PlanParameter param) {
-			this.plans = plans;
-			this.desires = desires;
-			this.pursued = pursued;
-			this.intentions = intentions;
-			this.param = param;
-		}
-
-		/**
-		 * @return the agent'S plan-component
-		 */
-		public PlanComponent getPlans() {
-			return plans;
-		}
-
-		/**
-		 * @return a set of all desires, the agent currently has
-		 */
-		public Set<Desire> getDesires() {
-			return desires;
-		}
-
-		/**
-		 * @return the currently pursued desire, if any
-		 */
-		public Desire getPursued() {
-			return pursued;
-		}
-
-		/**
-		 * @return a mapping between an intention and the desire it fulfills
-		 */
-		public Map<Desire, Subgoal> getIntentions() {
-			return intentions;
-		}
-
-		/**
-		 * @return the original plan-parameter
-		 */
-		public PlanParameter getParam() {
-			return param;
-		}
-
 	}
 
 }
