69915229c9755d825794d47e7c80e87441b716fe
Manuel Barbi <manuel.barbi@tu-dortmund.de>
2015-09-18 13:07:36 +0200

    removed param from SimpleBehavior and fixed tick is not counting

--- simple/src/main/java/com/github/angerona/fw/simple/behavior/SimpleBehavior.java
+++ simple/src/main/java/com/github/angerona/fw/simple/behavior/SimpleBehavior.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import com.github.angerona.fw.Action;
 import com.github.angerona.fw.Agent;
+import com.github.angerona.fw.Angerona;
 import com.github.angerona.fw.AngeronaEnvironment;
 import com.github.angerona.fw.Perception;
 import com.github.angerona.fw.def.DefaultBehavior;
@@ -14,7 +15,7 @@ import com.github.angerona.fw.def.DefaultBehavior;
  * @author Manuel Barbi
  *
  */
-public abstract class SimpleBehavior<P> extends DefaultBehavior {
+public abstract class SimpleBehavior extends DefaultBehavior {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SimpleBehavior.class);
 
@@ -22,39 +23,82 @@ public abstract class SimpleBehavior<P> extends DefaultBehavior {
 	public abstract void sendAction(AngeronaEnvironment env, Action act);
 
 	@Override
-	public void receivePerception(AngeronaEnvironment env, Perception percept) {}
+	public void receivePerception(AngeronaEnvironment env, Perception percept) {
+	}
 
 	@Override
 	public boolean runOneTick(AngeronaEnvironment env) {
-		P param = runEnvironment();
-		return runAgentCycles(env, param);
+		doingTick = true;
+
+		// simulation stuff
+
+		while (!isSimulationReady()) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		angeronaReady = false;
+		++tick;
+		Angerona.getInstance().onTickStarting(env);
+
+		// actual execution
+
+		runEnvironment();
+		somethingHappens = runAgentCycles(env);
+
+		// synchronization stuff
+
+		angeronaReady = true;
+
+		doingTick = false;
+		Angerona.getInstance().onTickDone(env);
+
+		return somethingHappens;
 	}
 
-	protected boolean runAgentCycles(AngeronaEnvironment env, P param) {
+	protected boolean runAgentCycles(AngeronaEnvironment env) {
 		boolean somethingHappend = false;
 		Perception perception;
 
 		for (Agent agent : env.getAgents()) {
-			if (cycleCondition(env, agent, param)) {
-				somethingHappend = true;
-				perception = createPerception(env, agent, param);
-				LOG.debug("create perception: {}", perception);
-				agent.perceive(perception);
-				LOG.debug("call agent cycle");
-				agent.cycle();
-				postCycle(env, agent, param);
+			if (cycleCondition(env, agent)) {
+				try {
+					somethingHappend = true;
+					perception = createPerception(env, agent);
+					LOG.debug("create perception: {}", perception);
+					agent.perceive(perception);
+					LOG.debug("call agent cycle");
+					agent.cycle();
+					postCycle(env, agent);
+				} catch (Exception e) {
+					e.printStackTrace();
+					// TODO: show some kind of message in gui
+				}
 			}
 		}
 
 		return somethingHappend;
 	}
 
-	protected abstract P runEnvironment();
+	/**
+	 * 
+	 * global execution previous to the agent's cycle
+	 */
+	protected abstract void runEnvironment();
 
-	protected abstract boolean cycleCondition(AngeronaEnvironment env, Agent agent, P param);
+	/**
+	 * 
+	 * @param env
+	 * @param agent
+	 * @return weather the agent's cycle is called
+	 */
+	protected abstract boolean cycleCondition(AngeronaEnvironment env, Agent agent);
 
-	protected abstract Perception createPerception(AngeronaEnvironment env, Agent agent, P param);
+	protected abstract Perception createPerception(AngeronaEnvironment env, Agent agent);
 
-	protected abstract void postCycle(AngeronaEnvironment env, Agent agent, P param);
+	protected abstract void postCycle(AngeronaEnvironment env, Agent agent);
 
 }
