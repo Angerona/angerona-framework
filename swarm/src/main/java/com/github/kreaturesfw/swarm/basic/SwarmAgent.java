da5b84410c17580e4fe2ae617848311bed442c92
Manuel Barbi <manuel.barbi@tu-dortmund.de>
2015-11-27 11:16:25 +0100

    created swarm project

---
+++ swarm/src/main/java/com/github/kreaturesfw/swarm/basic/SwarmAgent.java
@@ -0,0 +1,12 @@
+package com.github.kreaturesfw.swarm.basic;
+
+import com.github.kreaturesfw.core.basic.AbstractAgent;
+
+/**
+ * 
+ * @author Manuel Barbi
+ *
+ */
+public abstract class SwarmAgent extends AbstractAgent<SwarmPerception> {
+
+}
---
+++ swarm/src/main/java/com/github/kreaturesfw/swarm/basic/SwarmPerception.java
@@ -0,0 +1,18 @@
+package com.github.kreaturesfw.swarm.basic;
+
+import com.github.kreaturesfw.core.basic.Perception;
+
+/**
+ * 
+ * @author Manuel Barbi
+ *
+ */
+public class SwarmPerception implements Perception {
+
+	@Override
+	public String getReceiverId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
